7e0607216d0e910aa41a0ba3629619e7bf8f7f8c
Neha Chaudhary <neha.chaudhary@hotmail.com>
2018-01-25 12:11:27 -0800

    Link server to client

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 2828;
+const router = require('./routes');
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-})
+app.use(bodyParser.urlencoded());
+
+app.use(express.static('client'));
+
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 })
 
-module.exports = app;
+module.exports = {
+  app,
+}
